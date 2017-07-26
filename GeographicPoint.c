c0f7d0c8909551856bbba120fb339421f1de6640
Alejandra <ubuntu@alejandra>
2017-07-26 18:14:02 -0500

    Funcion de Numeros aleatorios

--- GeographicPoint.c
+++ GeographicPoint.c
@@ -1,40 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <time.h>
+
+//Funciones Declaradas
+int fil;
+int col;
+int **matrix(void);
+void n_aleatorios(int **matriz, int *a, int *b);
+
+//Main
 
 void main(){
 
-FILE *datos;
+fil = 500;
+col=744;
+int i, j ,c = 0;
+int x, y;
 
-int fil = 500;
-int col=744;
-int i, j = 0;
+int **matriz= matrix(); 
+n_aleatorios(matriz, &x, &y);
+printf("%d %d\n", x, y);
+}
 
-int **matriz= malloc(fil*col*sizeof(float)); 
+//Lectura de datos
 
-datos = fopen("map_data.txt", "r");
+int **matrix(void){
 
-for (i=0; i<fil;i++){
-	matriz[i]=malloc(744*sizeof(int));
+	int i;
+	int j;
+	int **Matrix= malloc(fil*col*sizeof(int));
+	FILE *datos;
+
+	datos = fopen("map_data.txt", "r");
+
+	for (i=0; i<fil;i++){
+	Matrix[i]=malloc(col*sizeof(int));
 	}
 
 
-for (i=0; i<fil;i++){
-	for (j=0;j<col;j++)
-		fscanf(datos, "%d", &matriz[i][j]);
+	for (i=0; i<fil;i++){
+		for (j=0;j<col;j++)
+			fscanf(datos, "%d", &Matrix[i][j]);
+	}
+	
+	fclose(datos);
+	return Matrix;
 }
+ 
+//Numeros Aleatorios
 
-for (i=0; i<fil;i++){
-	for (j=0;j<col;j++)
-		printf("%d\n", matriz[i][j]);
+void n_aleatorios(int **matriz, int *a, int *b){  
+
+	srand(time(NULL));
+
+	int j=*a;
+	int k=*b;
+	
+	int num_fil=rand() % (500+1);
+	int num_col=rand() % (744+1);
+	
+	while(matriz[num_fil][num_col]==1)
+	{
+		num_fil=rand() % (500+1);
+		num_col=rand() % (744+1);	
+	}
+	
+	j=num_fil;
+	k=num_col;
+	
+	*a=j;
+	*b=k;
 }
-
-}
-
-
-
-
-
 
 
 
