1b35bf09dc73d400edae1c08ebef1304abac06cf
Alejandra <ubuntu@alejandra>
2017-07-27 20:34:58 -0500

    Grafica con circulo

--- plots.py
+++ plots.py
@@ -1,7 +1,17 @@
 import numpy as np 
 import matplotlib.pyplot as plt
 
-mapa = np.genfromtxt('map_data.txt')
+map = np.genfromtxt('map_data.txt')
+datos=np.genfromtxt('datos.txt')
 
-plt.imshow(mapa, cmap="winter")
-plt.savefig("Mapa.pdf")
+rad=datos[0]
+py=datos[1]
+px=datos[2]
+
+fig, top=plt.subplots()
+circulo=plt.Circle((px,py), rad, alpha=.2, color='black')
+top.add_artist(circulo)
+top.add_artist(plt.imshow(map, cmap='winter'))
+top.set_title("PUNTO NEMO-MC")
+plt.savefig('PuntoNemo.pdf')
+plt.close()
